d1cd588c9980007539ca8e3623ea7f2e2b8d23da
17020731 <dinhhiepxtb@gmail.com>
2020-02-28 17:55:35 +0700

    add darkmode and language base

--- app/src/main/java/com/example/weatherapp/main/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/main/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
@@ -12,14 +14,23 @@ import androidx.viewpager.widget.ViewPager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -36,8 +47,10 @@ import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 
+import cn.pedant.SweetAlert.SweetAlertDialog;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -59,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
     private NavigationView navigationView;
     private CircleImageView avatar;
     private TextView name;
+    private SweetAlertDialog pDialog;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -92,6 +106,18 @@ public class MainActivity extends AppCompatActivity {
 
         sqlHelper = new SQLHelper(this);
         sp = getSharedPreferences("search", Context.MODE_PRIVATE);
+        editor = sp.edit();
+
+        String lang = sp.getString("lang", "en");
+        loadLocal(lang);
+
+        String mode = sp.getString("mode", "light");
+        if(mode.equals("light")){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+
 
         Database database = new Database(this);
         Set<City> set = database.getUsersList();
@@ -110,13 +136,17 @@ public class MainActivity extends AppCompatActivity {
         navigationView = findViewById(R.id.navigationView);
         navigationView.setItemIconTintList(null);
 
+
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 int id = menuItem.getItemId();
                 Intent intent;
                 switch (id) {
-
+                    case R.id.mode:
+                        SwitchCompat switchCompat = menuItem.getActionView().findViewById(R.id.switchCompat);
+                        setDayNightMode(switchCompat);
+                        break;
                     case R.id.home:
                         drawer.closeDrawer(Gravity.LEFT);
                         mViewPage.setCurrentItem(0);
@@ -132,10 +162,89 @@ public class MainActivity extends AppCompatActivity {
                         mViewPage.setCurrentItem(2);
                         break;
 
-                    case R.id.setting:
-                        drawer.closeDrawer(Gravity.LEFT);
-                        intent = new Intent(MainActivity.this, SettingActivity.class);
-                        startActivity(intent);
+                    case R.id.language:
+                        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        View viewInflated = LayoutInflater.from(MainActivity.this).inflate(R.layout.dialog_language, navigationView, false);
+                        builder.setView(viewInflated);
+                        final AlertDialog dialog = builder.create();
+
+                        ImageView btnClose = viewInflated.findViewById(R.id.btnClose);
+                        btnClose.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                dialog.dismiss();
+                            }
+                        });
+
+                        RelativeLayout btnEn = viewInflated.findViewById(R.id.btnEn);
+                        RelativeLayout btnJa = viewInflated.findViewById(R.id.btnJa);
+
+                        final ImageView checkEn = viewInflated.findViewById(R.id.checkEn);
+                        final ImageView checkJa = viewInflated.findViewById(R.id.checkJa);
+
+                        if (sp.getString("lang", "en").equals("en")) {
+                            checkEn.setVisibility(View.VISIBLE);
+                            checkJa.setVisibility(View.INVISIBLE);
+
+                        } else if (sp.getString("lang", "en").equals("ja")) {
+                            checkJa.setVisibility(View.VISIBLE);
+                            checkEn.setVisibility(View.INVISIBLE);
+                        }
+
+                        btnEn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                checkEn.setVisibility(View.VISIBLE);
+                                checkJa.setVisibility(View.INVISIBLE);
+
+                            }
+                        });
+                        btnJa.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                checkJa.setVisibility(View.VISIBLE);
+                                checkEn.setVisibility(View.INVISIBLE);
+                            }
+                        });
+
+                        TextView btnSubmit = viewInflated.findViewById(R.id.btnSubmit);
+                        btnSubmit.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                String lang_check = "";
+                                if(checkEn.getVisibility() == View.VISIBLE){
+                                    lang_check = "en";
+                                } else if(checkJa.getVisibility() == View.VISIBLE){
+                                    lang_check = "ja";
+                                }
+                                final SweetAlertDialog alertDialog = new SweetAlertDialog(MainActivity.this, SweetAlertDialog.NORMAL_TYPE);
+                                final String finalLang_check = lang_check;
+                                alertDialog.setContentText("Do you want to change your language?")
+                                        .setConfirmText("YES")
+                                        .setCancelText("CANCEL")
+                                        .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                                            @Override
+                                            public void onClick(SweetAlertDialog sweetAlertDialog) {
+                                                editor.putString("lang", finalLang_check);
+                                                editor.commit();
+                                                changeLanguage(finalLang_check);
+                                                dialog.dismiss();
+                                                alertDialog.dismiss();
+                                            }
+                                        })
+                                        .setCancelClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                                            @Override
+                                            public void onClick(SweetAlertDialog sweetAlertDialog) {
+                                                alertDialog.dismiss();
+                                            }
+                                        })
+                                        .showCancelButton(true)
+                                        .show();
+
+
+                            }
+                        });
+                        dialog.show();
                         break;
 
                     case R.id.about:
@@ -149,6 +258,10 @@ public class MainActivity extends AppCompatActivity {
                 }
                 return false;
             }
+
+            private void changeLanguage(String finalLang_check) {
+            }
+
         });
         btnMenu = findViewById(R.id.btnMenu);
         btnMenu.setOnClickListener(new View.OnClickListener() {
@@ -167,7 +280,7 @@ public class MainActivity extends AppCompatActivity {
                         int index = city_name.indexOf(name);
                         String id = data.get(index).getId();
                         autoSearch.setText("");
-                        editor = sp.edit();
+
 
                         editor.putString("id", id);
                         editor.putString("name", name);
@@ -182,11 +295,65 @@ public class MainActivity extends AppCompatActivity {
 
                     } else {
                         autoSearch.setText("");
-                        Toast.makeText(getApplicationContext(), "Không có dữ liệu!!\n Mời nhập lại!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Not existed!!\n Please reenter!", Toast.LENGTH_SHORT).show();
                     }
                 }
             }
         });
 
+    }
+
+    private void setDayNightMode(SwitchCompat swMode){
+        String mode = sp.getString("mode", "light");
+        if(mode.equals("light")){
+            swMode.setChecked(false);
+        } else {
+            swMode.setChecked(true);
+        }
+        swMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    editor.putString("mode", "dark");
+                    editor.commit();
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
+                } else {
+                    editor.putString("mode", "light");
+                    editor.commit();
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+            }
+        });
+    }
+
+    public void changeLanguage(String lang){
+        loadLocal(lang);
+
+        pDialog = new SweetAlertDialog(MainActivity.this, SweetAlertDialog.PROGRESS_TYPE);
+        pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+        pDialog.setTitleText("Loading");
+        pDialog.setCancelable(false);
+        pDialog.show();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                pDialog.dismiss();
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        }, 1000);
+    }
+    public void loadLocal(String lang) {
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            conf.setLocale(new Locale(lang)); // API 17+ only.
+        }
+        res.updateConfiguration(conf, dm);
     }
 }
